30db42946235ed928154594f5542353a0e711b8a
ErnestMakarov <ernest.makarov@tptlive.ee>
2026-09-02 17:33:55 +0300

    -

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -5,6 +5,9 @@ import HomePage from "../pages/Home/Home.jsx";
 import AboutPage from "../pages/About/About.jsx";
 import ContactsPage from "../pages/Contacts/Contacts.jsx";
 import NotFound from "../pages/NotFound/NotFound.jsx";
+import Work from "../pages/Work/Work.jsx";
+import Services from "../pages/Services/Services.jsx";
+import Process from "../pages/Process/Process.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +16,18 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/work",
+        element: <Work />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/process",
+        element: <Process />,
       },
       {
         path: "/about",
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="p-4 bg-gray-200 text-center">
-      &copy; {new Date().getFullYear()} My App. All rights reserved.
+      &copy; {new Date().getFullYear()} Ernest Makarov. All rights reserved.
     </div>
   )
 }
